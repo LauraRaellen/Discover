12a90a442a2dbd88bb36c86c73bcaf9db513bfe2
Laura Raellen <lauraraellen0@gmail.com>
2022-02-11 23:37:43 -0300

    update geral do repositorio

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Discover
+# Discorve
+Rocketseat
--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
-const express = require (`express`)
-const { route } = require('express/lib/application')
-
-const houte = require('./route')
+const express = require('express')
+const route = require('./route')                 
+const path = require('path')
 
 const server = express()
 
 server.set('view engine', 'ejs')
 
-
-
 server.use(route)
 
 server.listen(3000, () => console.log("RODANDO"))
+
