557354c96d547ab41fe0baa88ca6306b3c56356f
Laura Raellen <lauraraellen0@gmail.com>
2022-02-11 23:13:33 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -1,16 +1,10 @@
 const express = require('express')
-const route = require('route')                 
+const route = require('./route')                 
 const path = require('path')
 
 const server = express()
 
 server.set('view engine', 'ejs')
-
-server.use(express.static("public"))
-
-server.set('views', path.join(__dirname, 'views'))
-
-server.use(express.urlencoded({extended: true}))
 
 server.use(route)
 
