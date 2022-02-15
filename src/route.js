b0c406435ee30e34418e7d2df65c43108b271929
Laura Raellen <lauraraellen0@gmail.com>
2022-02-14 23:23:57 -0300

    Teste

--- src/route.js
+++ src/route.js
@@ -3,6 +3,9 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))
+route.get('/create-pass', (req,res) => res.render("index"))
 
 module.exports = route
+
+
 
